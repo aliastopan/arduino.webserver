28c2f9fab009a751f0b7c40c659742232aaa46c9
Taufan Augusta <taufan.augusta@protonmail.com>
2021-12-08 15:59:28 +0700

    refactor: add _server.h

---
+++ include/_server.h
@@ -0,0 +1,3 @@
+const char* ssid = "Wifi@Home";
+const char* password = "02062000";
+const int port = 80;
--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
-const char* ssid = "Wifi@Home";
-const char* password = "02062000";
-const int port = 80;
+#include "_server.h"
+
+// const char* ssid = "Wifi@Home";
+// const char* password = "02062000";
+// const int port = 80;
 
 AsyncWebServer server(port);
 

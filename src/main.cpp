79f164b2d8c2cf4f357c28c9c92e3a5c0abf56cd
einharan <taufan.augusta@protonmail.com>
2021-12-08 13:41:47 +0700

    feat: add filesystem

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <LittleFS.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void flashSystem(const char* _file)
+{
+	if (!LittleFS.begin())
+	{
+		Serial.println("[error] failed to mount LittleFS");
+		return;
+	}
+
+	File file = LittleFS.open(_file, "r");
+	if (!file)
+	{
+		Serial.println("[error] failed to open file");
+		return;
+	}
+
+	Serial.println("file content: ");
+	while (file.available())
+	{
+		Serial.write(file.read());
+	}
+	file.close();
+
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void setup()
+{
+	Serial.begin(115200);
+	flashSystem("/sample.txt");
+}
+
+void loop()
+{
+
 }

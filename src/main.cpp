892793c481f51b3b858eac519f6828be6b580ed5
einharan <taufan.augusta@protonmail.com>
2021-12-08 14:03:29 +0700

    feat: add wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <Arduino.h>
 #include <LittleFS.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+const char* ssid = "Wifi@Home";
+const char* password = "02062000";
+const int port = 80;
+
+AsyncWebServer server(port);
 
 void flashSystem(const char* _file)
 {
@@ -31,6 +40,16 @@ void setup()
 {
 	Serial.begin(115200);
 	flashSystem("/sample.txt");
+
+	// Connect to Wi-Fi
+  	WiFi.begin(ssid, password);
+  	while (WiFi.status() != WL_CONNECTED) 
+	{
+    	delay(1000);
+    	Serial.println("Connecting to WiFi..");
+  	}
+
+  	Serial.println(WiFi.localIP());
 }
 
 void loop()
